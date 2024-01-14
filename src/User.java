88ff3db16802cdb6f1cf92a98e1aa46abd1765ab
adelinbojan96 <113366480+adelinbojan96@users.noreply.github.com>
2024-01-14 18:49:24 +0200

    Update User

--- src/User.java
+++ src/User.java
@@ -1,30 +1,30 @@
 import javax.swing.*;
 
 public record User(int id, String username, String password, String email, String phone, byte[] image) {
-    private static User currentUser;  // static field to store the current user
-    // private constructor to prevent direct instantiation
+    private static User currentUser;  
+    
 
-    // public method to get the current user
+    
     public static User getCurrentUser() {
         return currentUser;
     }
 
-    // public method to set the current user
+    
     public static void setCurrentUser(User user) {
         currentUser = user;
     }
 
-    // public method to check if a user is logged in
+    
     public static boolean isLoggedIn() {
         return currentUser != null;
     }
 
-    // public method to log out the current user
+    
     public static void logout() {
         currentUser = null;
     }
 
-    // Getter methods for user attributes
+    
     public ImageIcon getImageIcon() {
         return (image != null) ? new ImageIcon(image) : null;
     }
@@ -35,7 +35,7 @@ public record User(int id, String username, String password, String email, Strin
     }
 
 
-    // Factory method to create a new user instance
+
     public static User createUser(int id, String username, String password, String email, String phone, byte[] image) {
         return new User(id, username, password, email, phone, image);
     }
