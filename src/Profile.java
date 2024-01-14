59b4445d64165a94571c49b35b978c4304e474dc
adelinbojan96 <113366480+adelinbojan96@users.noreply.github.com>
2024-01-14 19:04:29 +0200

    Update Profile.java

--- src/Profile.java
+++ src/Profile.java
@@ -41,21 +41,21 @@ public class Profile extends JDialog {
         setModal(true);
         setLocationRelativeTo(parent);
 
-        // Create a border for the image and set it if not null
+        //create a border for the image and set it if not null
         Border emptyBorder = BorderFactory.createLineBorder(Color.BLACK, 5);
         profileImage.setBorder(emptyBorder);
 
-        // Display the image if it exists in the database
+        //display the image if it exists in the database
         if (User.isLoggedIn()) {
             int currentId = User.getCurrentUser().id();
-            // Retrieve the image data from the database
+            //retrieve the image data from the database
             byte[] imageData = retrieveImageDataFromDatabase(currentId);
 
-            // If the image data is not null, update the profileImage
+            //if the image data is not null, update the profileImage
             if (imageData != null)
                 profileImage.setIcon(resizeImage(imageData));
 
-            // If messages >=0, display
+            //if messages >=0, display
             int messages = retrieveNumberOfMessagesFromTheDatabase(currentId);
             if (messages >= 0) {
                 numberOfMessages = messages;
@@ -63,14 +63,14 @@ public class Profile extends JDialog {
             }
         }
 
-        // Add customization for the 2 buttons
+        //add customization for the 2 buttons
         customizeButton(buttonPicture);
         customizeButton(buttonEdit);
 
-        // Set user information
+        //set user information
         setUserInformation();
 
-        // Go back to AnimalDisplayScreen
+        //go back to AnimalDisplayScreen
         goToAnimalDisplayScreen.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -79,26 +79,26 @@ public class Profile extends JDialog {
                 new AnimalDisplayScreen(null);
             }
         });
-        // Set new image from local
+        //set new image from local
         buttonPicture.addActionListener(e -> {
-            // Display a file chooser dialog
+            //display file chooser dialog
             JFileChooser fileChooser = new JFileChooser();
             int result = fileChooser.showOpenDialog(Profile.this);
 
             if (result == JFileChooser.APPROVE_OPTION) {
-                // Get the selected file
+                //get the selected file
                 File selectedFile = fileChooser.getSelectedFile();
 
                 try {
-                    // Check if the selected file is an image
+                    //check if the selected file is an image
                     if (isImageFile(selectedFile)) {
-                        // Convert the selected image file to a byte array
+                        //convert the selected image file to a byte array
                         byte[] imageData = Files.readAllBytes(selectedFile.toPath());
 
-                        // Update the user's profile image in the UI
+                        //update the user's profile image in the UI
                         profileImage.setIcon(resizeImage(imageData));
 
-                        // Update the pet's image in the database
+                        //update the pet's image in the db
                         updateUserImageInDatabase(User.getCurrentUser().id(), imageData, parent);
                     } else {
                         JOptionPane.showMessageDialog(Profile.this, "Invalid file format. Please select an image file.", "Error", JOptionPane.ERROR_MESSAGE);
@@ -170,10 +170,10 @@ public class Profile extends JDialog {
                         int rowsAffected = updateStatement.executeUpdate();
 
                         if (rowsAffected > 0) {
-                            // Image update successful
+                            // image update successful
                             JOptionPane.showMessageDialog(parent, "Image update successful");
                         } else {
-                            // No rows were affected, update failed
+                            // no rows were affected, update failed
                             JOptionPane.showMessageDialog(parent, "Image update failed");
                         }
                     } catch (SQLException e) {
@@ -181,12 +181,11 @@ public class Profile extends JDialog {
                         e.printStackTrace();
                     }
                 } else {
-                    // User does not exist
+                    // user does not exist
                     JOptionPane.showMessageDialog(parent, "A user with this id does not exist");
                 }
             }
         } catch (SQLException e) {
-            // Handle database connection or query execution errors
             e.printStackTrace();
         }
     }
@@ -205,31 +204,31 @@ public class Profile extends JDialog {
     }
 
     private void setUserInformation() {
-        // Check if the user is logged in
+        //check if the user is logged in
         if (User.isLoggedIn()) {
-            // Set user information using database or sample data
+            //set user information using database or sample data
             int numberOfLettersPassword = User.getCurrentUser().password().length();
             String maskedPassword = "*".repeat(numberOfLettersPassword);
             usernameProfile.setText("User: " + User.getCurrentUser().username());
             passwordProfile.setText("Password: " + maskedPassword);
             emailProfile.setText("Email: " + User.getCurrentUser().email());
             phoneProfile.setText("Phone number: " + User.getCurrentUser().phone());
-            //If not null, we display it
+            //if not null, we display it
             ImageIcon imageIcon = User.getCurrentUser().getImageIcon();
             if (imageIcon != null) {
-                // Resize the image
+                //resize the image
                 Image originalImage = imageIcon.getImage();
                 Image resizedImage = originalImage.getScaledInstance(240, 180, Image.SCALE_SMOOTH);
                 profileImage.setIcon(new ImageIcon(resizedImage));
             }
 
-            // Update the user
+            //update the user
             User user = User.createUser(User.getCurrentUser().id(), User.getCurrentUser().username(),
                     User.getCurrentUser().password(), User.getCurrentUser().email(),
                     User.getCurrentUser().phone(), retrieveImageDataFromDatabase(User.getCurrentUser().id()));
             User.setCurrentUser(user);
         } else {
-            // Set default or sample data if the user is not logged in
+            //set default or sample data if the user is not logged in
             usernameProfile.setText("User: Guest");
             emailProfile.setText("Email: Email");
             phoneProfile.setText("Phone number: No Phone Number");
@@ -244,7 +243,7 @@ public class Profile extends JDialog {
                 preparedStatement.setInt(1, userId);
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     if (resultSet.next()) {
-                        // Retrieve the image data from the result set
+                        //retrieve the image data from the result set
                         return resultSet.getBytes("user_image");
                     }
                 }
@@ -277,7 +276,6 @@ public class Profile extends JDialog {
     }
 
     private ImageIcon resizeImage(byte[] imageData) {
-        // Resize the image
         ImageIcon imageIcon = new ImageIcon(imageData);
         Image originalImage = imageIcon.getImage();
         Image resizedImage = originalImage.getScaledInstance(240, 180, Image.SCALE_SMOOTH);
@@ -285,30 +283,30 @@ public class Profile extends JDialog {
     }
 
     private void updateColor(String color, JFrame parent) {
-        //Here the id will come in our aid
+        //here the id will come in our aid
         DatabaseManager databaseManager = new DatabaseManager();
         try (Connection connection = databaseManager.getConnection()) {
-            // Check if the user exists
+            //check if the user exists
             String selectQuery = "SELECT color_preferred FROM users WHERE id_user = ?";
             try (PreparedStatement selectStatement = connection.prepareStatement(selectQuery)) {
                 selectStatement.setInt(1, User.getCurrentUser().id());
                 try (ResultSet resultSet = selectStatement.executeQuery()) {
                     if (resultSet.next()) {
-                        // User is found, update the color
+                        //user is found, update the color
                         String updateQuery = "UPDATE users SET color_preferred = ? WHERE id_user = ?";
                         try (PreparedStatement updateStatement = connection.prepareStatement(updateQuery)) {
-                            //We check what to update in the database
+                            //we check what to update in the database
 
                             updateStatement.setString(1, color);
                             updateStatement.setInt(2, User.getCurrentUser().id());
-                            // Execute the update statement
+                            //execute the update statement
                             int rowsAffected = updateStatement.executeUpdate();
 
                             if (rowsAffected > 0) {
-                                // Color update was successful
+                                //color update was successful
                                 JOptionPane.showMessageDialog(parent, "Edits were successful");
                             } else {
-                                // No rows were affected, update failed
+                                //no rows were affected, update failed
                                 JOptionPane.showMessageDialog(parent, "Edits have failed");
                             }
                         } catch (SQLException e) {
@@ -322,7 +320,6 @@ public class Profile extends JDialog {
                 }
             }
         } catch (SQLException e) {
-            // Handle database connection or query execution errors
             e.printStackTrace();
         }
     }
